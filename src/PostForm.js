73e8081730a78622467749e831ef9d595ea93b67
Michael Hurley <mike@swampsoftware.org>
2018-10-10 11:37:36 -0400

    Part two: delete and update

--- src/PostForm.js
+++ src/PostForm.js
@@ -8,26 +8,25 @@ class PostForm extends Component {
     const data = {
       id: new Date(),
       title,
-      message
+      message,
+      editing: false
     }
     this.props.dispatch({
       type: 'ADD_POST',
       data
-    });
+    })
     this.getTitle.value = '';
     this.getMessage.value = '';
   }
   render() {
     return (
-      <div>
-        <h1>Create Post</h1>
-        <form onSubmit={this.handleSubmit}>
+      <div className="post-container">
+        <h1 className="post_heading">Create Post</h1>
+        <form className="form" onSubmit={this.handleSubmit} >
           <input required type="text" ref={(input) => this.getTitle = input}
-            placeholder="Enter Post Title" />
-          <br /><br />
-          <textarea required rows="5" ref={(input) => this.getMessage = input} cols="28"
-            placeholder="Enter Post" />
-          <br /><br />
+            placeholder="Enter Post Title" /><br /><br />
+          <textarea required rows="5" ref={(input) => this.getMessage = input}
+            cols="28" placeholder="Enter Post" /><br /><br />
           <button>Post</button>
         </form>
       </div>
